847aabb51167f7e81dcff857d71d837b98fbc5fb
vinaysharma <vs864606@gmail.com>
2025-03-28 14:59:44 +0530

    delete

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use(session(sessionOption));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname,"public")));
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
